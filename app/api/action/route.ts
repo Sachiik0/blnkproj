175011387789c65bae1f70598d12bfbacdad8173
Sachiik0 <mmasanjuan@tip.edu.ph>
2024-07-20 13:44:18 +0800

    update

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -2,7 +2,7 @@ import { ActionGetResponse, ActionPostRequest, ActionPostResponse, ACTIONS_CORS_
 
 export async function GET(request: Request) {
   const responseBody : ActionGetResponse = {
-    icon: '',
+    icon: 'https://scontent.fceb1-5.fna.fbcdn.net/v/t39.30808-6/451298361_431465133272016_4432019430255871475_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeHmrkNrk_3Z3VY_6O0H9ibVGkgJYCYrJZAaSAlgJislkLOM_LQ0w8bxx23aCCLkIkmoEV_ar3dk4rDGcTDiP4Gy&_nc_ohc=IPFSzIFZ1G0Q7kNvgFQF4Y0&_nc_ht=scontent.fceb1-5.fna&oh=00_AYDQZeMlkZ0dFmkKuUF1okMlfozWl-BkSVznF_p2Zu4Fog&oe=66A130F4',
     description: 'test blink',
     title: 'Do Blink',
     label: 'Click Me!'
