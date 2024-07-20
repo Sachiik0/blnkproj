2813d6312187c19fb84de868cde242799b87c10c
Sachiik0 <mmasanjuan@tip.edu.ph>
2024-07-20 14:21:48 +0800

    update

--- app/api/action/route.ts
+++ app/api/action/route.ts
@@ -14,7 +14,7 @@ export const GET = async (req: Request) => {
 
     const payload: ActionGetResponse = {
       icon: "https://scontent.fceb1-5.fna.fbcdn.net/v/t39.30808-6/451298361_431465133272016_4432019430255871475_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=aa7b47&_nc_eui2=AeHmrkNrk_3Z3VY_6O0H9ibVGkgJYCYrJZAaSAlgJislkLOM_LQ0w8bxx23aCCLkIkmoEV_ar3dk4rDGcTDiP4Gy&_nc_ohc=IPFSzIFZ1G0Q7kNvgFQF4Y0&_nc_ht=scontent.fceb1-5.fna&oh=00_AYDQZeMlkZ0dFmkKuUF1okMlfozWl-BkSVznF_p2Zu4Fog&oe=66A130F4",
-      description: "Sino Manalo?",
+      description: "''Palimos'' is a Filipino term that refers to begging or the act of asking for alms or charity, typically on the streets or in public places. It often involves individuals or groups asking passersby for money or food to meet their immediate needs. Palimos is a common sight in many urban areas in the Philippines, where poverty and economic hardships may drive people to seek assistance from others.",
       title: "SSX Tricky Pustahan sa EDSA Busway",
       label: "Click Me!", // this value will be ignored since `links.actions` exists
       links: {
